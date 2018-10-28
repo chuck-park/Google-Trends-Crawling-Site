11498e16443af87650b291d817809690a45fa80c
Chungmin Park(Chuck) <chuck.m.park@gmail.com>
2018-10-28 14:16:56 +0900

    change href system(absolute -> relative)

--- routes/crawler.js
+++ routes/crawler.js
@@ -9,26 +9,7 @@ const categoryNum = 8;
 const titleNum = 10;
 
 router.get('/', catchErrors(async (req, res, next) => {
-
-  // const url = 'https://trends.google.com/trends/topcharts/category';
-
-  // request.post({url:url, form:{ajax:1, date:2017, geo:'KR'}}, function(error, response, body) {
-  //   if(!error) {
-  //     let obj = JSON.parse(body);
-  //     let arr = new Array(categoryNum);
-  //     for(let i = 0; i < categoryNum; i++) {
-  //       arr[i] = new Array(titleNum); 
-  //     }
-  //     for(let i = 0; i < categoryNum; i++) {
-  //       arr[i][0] = obj.data.chartList[i].trendingChart.visibleName
-  //       for (let j = 0; j < titleNum; j++) {
-  //         arr[i][j+1] = obj.data.chartList[i].trendingChart.entityList[j].title
-  //       }
-  //     }
-  //     res.render('crawler/index', {arrs:arr, obj:obj})
-  //   }
-  // })
-
+  
 }));
 
 
